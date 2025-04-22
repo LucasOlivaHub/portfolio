b79634a87ffde2b139224814a39d113ff80289e4
Lucas <lucasolivah@gmail.com>
2025-04-22 09:17:27 -0300

    Update: gap navbar

--- src/Components/Page404/Page404.js
+++ src/Components/Page404/Page404.js
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react'
 import { backgroundContext } from '../../Context/BackgroundContext';
-import { Link } from 'react-router-dom';
 
 export const Page404 = () => {
-    const {language, textColors, subtitleColors, labelColors} = useContext(backgroundContext);
+    const {language, textColors} = useContext(backgroundContext);
     
   return (
     <section className='pagina404-section'>
